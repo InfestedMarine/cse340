aa4f55ac805bb764cb78a2d004da75286cece575
InfestedMarine <cyandsanchez@gmail.com>
2025-09-08 01:08:58 +0800

    I MADE A SITE OUT OF NODE

--- server.js
+++ server.js
@@ -12,17 +12,20 @@ const app = express()
 const static = require("./routes/static")
 
 /* ***********************
- * Routes
- *************************/
-app.use(static)
-
-
-/* ***********************
  * View Engine and Templates
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
+app.set("layout", "./layouts/layout.ejs") // not at views root
+
+
+/* ***********************
+ * Routes
+ *************************/
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
 
 /* ***********************
  * Local Server Information
@@ -39,8 +42,8 @@ app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
 
-
 /* ******************************************
  * Default GET route
  * ***************************************** */
-app.get("/", (req, res) => {res.send("Welcome home!")})
+app.get("/", (req, res) => {res.render('index')})
+
